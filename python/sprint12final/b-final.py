d42682a6a119c6a3819d664feb289360c621a4f7
Yuri Troyanov <107502951+troyanoff@users.noreply.github.com>
2022-11-24 10:57:05 +0300

    Update b-final.py

--- python/sprint12final/b-final.py
+++ python/sprint12final/b-final.py
@@ -1,33 +1,47 @@
+# 76009632
 class Stack:
+    # Создаем список примежуточных результатов (СПР).
     def __init__(self):
         self.result = []
-        
+    
+    # Метод добавляет числа в СПР.
     def push(self, value):
         self.result.append(value)
-        
-    def action(sign):
+    
+    # Метод производит арифметическую операцию исходя из входящего знака.
+    def action(self, sign):
+        # "Отрезаем" последний элемент СПР, второй по порядку вхождения в СПР.
         second = self.result.pop()
+        # Еще раз "отрезаем" эл-т. Он первый по порядку вхождения в СПР.
         first = self.result.pop()
+        # Производим операцию с подготовленными эл-ми СПР исходя из знака на входе.
         if sign == '*':
             self.result.append(first * second)
         elif sign == '/':
-            self.result.append(first / second)
+            self.result.append(first // second)
         elif sign == '+':
             self.result.append(first + second)
         elif sign == '-':
             self.result.append(first - second)
     
-    def return_result():
+    # Метод возвращает промежуточный/конечный результат из СПР.
+    def return_result(self):
         return self.result[-1]
 
+# Функция для расчета результата входящего выражения.
 def calculation(data):
     stack = Stack()
+    # Заранее определяем список знаков.
     list_sign = '*/+-'
     for value in data:
+        # Проверяем является ли элемент знаком.
         if value in list_sign:
+            # Если да, то производим мат. операцию.
             stack.action(value)
         else:
-            stack.push(value)
+            # Иначе, просто добавляем в СПР.
+            stack.push(int(value))
+    # После перебора всех элементов входящего выражения, выводим результат.
     return stack.return_result()
         
 
@@ -35,7 +49,9 @@ def read_input():
     return input().strip().split()
 
 def main():
+    # Считываем входящие данные.
     data = read_input()
+    # Печатаем получившийся результат.
     print(calculation(data))
 
 if __name__ == '__main__':
