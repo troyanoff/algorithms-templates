bf8b2730d45620925b3c232ebe04b5a29559834d
troyanoff <troyan0v@yandex.ru>
2022-12-07 22:42:23 +0300

    1234

--- python/sprint13final/b-final.py
+++ python/sprint13final/b-final.py
@@ -1,48 +1,78 @@
-def sort_check_left(num,pivot):
-    if int(num[1]) == int(pivot[1]):
-        if int(num[2]) == int(pivot[2]):
-            return num[0] < pivot[0]
+# 78957608
+def check_left(participant, pivot):
+    """Проверяет, что участник сильнее опорного."""
+    # Распаковываем информацию, для экономии времени.
+    part_name, part_result, part_fine = participant
+    pivot_name, pivot_result, pivot_fine = pivot
+    # При равном кол-ве решенных задач, проверяем штраф.
+    if part_result == pivot_result:
+        # При равном штрафе, проверяем насколько сильно их имя.
+        if part_fine == pivot_fine:
+            # Чем unicode-шифр больше, тем слабее имя.
+            return part_name < pivot_name
         else:
-            return int(num[2]) > int(pivot[2])
-    return int(num[1]) < int(pivot[1])
+            # Если штраф меньше, участник сильнее.
+            return part_fine < pivot_fine
+    # Если предыдущий блок пропущен, проверяем кол-во задач.
+    return part_result > pivot_result
 
-def sort_check_right(num, pivot):
-    if int(num[1]) == int(pivot[1]):
-        if int(num[2]) == int(pivot[2]):
-            return num[0] > pivot[0]
+def check_right(participant, pivot):
+    """Проверяет, что участник слабее опорного."""
+    # Логика обратная той, что в функции check_left.
+    part_name, part_result, part_fine = participant
+    pivot_name, pivot_result, pivot_fine = pivot
+    if part_result == pivot_result:
+        if part_fine == pivot_fine:
+            return part_name > pivot_name
         else:
-            return int(num[2]) < int(pivot[2])
-    return int(num[1]) > int(pivot[1])
+            return part_fine > pivot_fine
+    return part_result < pivot_result
 
-def megasort(n, arr):
-    pivot = arr[n//2]
-    left = 0
-    right = n-1
-    while left != right
-        arr_left = arr[left]
-        arr_pivot = arr[pivot]
-        arr_right = arr[right]
-        while sort_check_left(arr[left], arr[pivot]):
-            left += 1
-        while sort_check_right(arr[right], arr[pivot]):
-            right -= 1
-        arr[left], arr[right] = arr[right], arr[left]
-    if n == 1:
+def econ_quick_sort(count, arr):
+    """Сортирует участников по убыванию их результатов."""
+    # Если массив стал пустым или с одним эл-том, возвращаем результат.
+    if count < 2:
         return arr
-    return megasort()
-    
+    # Определяем опорный индекс. Брать центральный == рандом.
+    pivot = count // 2
+    # Определяем индексы окончаний списка участников.
+    left = 0
+    right = count - 1
+    # Запоминаем опорный элемент.
+    arr_pivot = arr[pivot]
+    # След. блок будет выполняться пока не сойдутся указатели окончаний.
+    while right != left:
+        # Двигаем левый указатель, пока он соответсвует условиям.
+        while check_left(arr[left], arr_pivot):
+            left += 1
+        # Аналогично правый.
+        while check_right(arr[right], arr_pivot):
+            right -= 1
+        # Если нашлись не соответсвующие условиям эл-ты, меняем их местами.
+        arr[left], arr[right] = arr[right], arr[left]
+    # Соединяем массив тех, кто сильнее, с теми, кто слабее.
+    # В качестве длинны массива на левую сторону передаем указатель(они равны),
+    # а на правый, оставшееся до длинный массива число эл-тов.
+    return (econ_quick_sort(right, arr[:right])
+            + econ_quick_sort(count - right, arr[right:]))
 
 def read_input():
-    n = int(input())
-    arr = []
-    for _ in range(n):
-        arr.append(input().strip().split())
-    return n, arr
+    count = int(input())
+    participants = []
+    for _ in range(count):
+        # Во избежание множественных переводов в int в функциях проверки,
+        # сразу изменяем тип данных у кол-ва задач и штрафа.
+        participant = input().strip().split()
+        participant[1] = int(participant[1])
+        participant[2] = int(participant[2])
+        participants.append(participant)
+    return count, participants
 
 def main():
-    n, arr = read_input()
-    result = megasort(n, arr)
-    print(*result)
+    count, participants = read_input()
+    result = econ_quick_sort(count, participants)
+    for participant in result:
+        print(participant[0])
 
 if __name__ == '__main__':
     main()
