fcace03fdc88c9719a880b3d602d224efcc92fdf
Yuri Troyanov <107502951+troyanoff@users.noreply.github.com>
2022-12-07 13:03:01 +0300

    Update a-final.py

--- python/sprint13final/a-final.py
+++ python/sprint13final/a-final.py
@@ -1,16 +1,33 @@
 def search_target(nums, target, left, right):
-    len_nums = len(nums)
+    len_nums = right - left
     mid = len_nums // 2
+    nums_mid = nums[mid]
     if len_nums == 0:
         return -1
     elif len_nums == 1 and nums[0] != target:
         return -1
-    elif nums[mid] == target:
+    elif nums_mid == target:
         return mid + left
-    elif nums[mid] > target:
+    elif nums_mid > target:
         return search_target(nums[:mid], target, left, left + mid)
     else:
         return search_target(nums[mid:], target, left + mid, right)
+
+def search_start(nums, left, right):
+    len_nums = right - left
+    mid = len_nums // 2
+    nums_mid = nums[mid]
+    if len_nums == 1:
+        return nums[0]
+    elif len_nums == 0:
+        return "ашипка"
+    elif nums_mid < nums[mid-1]:
+        return left + mid
+    else:
+        if nums[-1] > nums_mid:
+            return search_start(nums[:mid], left, left + mid)
+        else:
+            return search_start(nums[mid:], left + mid, right)
 
 def check_broken(arr):
     return arr[0] > arr[-1]
@@ -22,35 +39,45 @@ def check_target(arr, target, broken):
         return arr[0] <= target <= arr[-1]
 
 def normal_search(nums, target, left, right):
-    mid = len(nums) // 2
+    mid = (right - left) // 2
     if not check_broken(nums):
         return nums, left, right
-    broken_left = check_broken(nums[:mid])
-    left_target = check_target(nums[:mid], target, broken_left)
+    nums_mid = nums[:mid]
+    nums_mid_ = nums[mid:]
+    broken_left = check_broken(nums_mid)
+    left_target = check_target(nums_mid, target, broken_left)
     if broken_left:
         if left_target:
-            return normal_search(nums[:mid], target, left, mid + left)
+            return normal_search(nums_mid, target, left, mid + left)
         else:
-            return nums[mid:], mid + left, right
+            return nums_mid_, mid + left, right
     else:
         if left_target:
-            return nums[:mid], left, mid + left
+            return nums_mid, left, mid + left
         else:
-            return normal_search(nums[mid:], target, mid + left, right)
+            return normal_search(nums_mid_, target, mid + left, right)
 
 def broken_search(nums, target) -> int:
     len_nums = len(nums)
-    mid = len_nums // 2
-    normal, left, right = normal_search(nums, target, 0, len_nums)
-    if len(normal) == 0:
-        return -1
+    start = search_start(nums, 0, len_nums)
+    if nums[0] > nums[start]:
+        return search_target(nums[start:], target, start, len_nums)
     else:
-        return search_target(normal, target, left, right)
+        return search_target(nums[:start], target, 0, start)
+    # normal, left, right = normal_search(nums, target, 0, len_nums)
+    # try:
+    #     return normal.index(target) + left
+    # except:
+    #     return -1
+    # if len(normal) == 0:
+    #     return -1
+    # else:
+    #     return search_target(normal, target, left, right)
 
 def read_input():
-    n = 9
+    n = 14
     target = 5
-    arr = [19, 21, 100, 101, 1, 4, 5, 7, 12]
+    arr = [19, 21, 100, 101, 102, 1, 5, 7, 12, 13, 14, 15, 16, 17]
     #n = int(input())
     #target = int(input())
     #arr = [int(value) for value in input().strip().split()]
